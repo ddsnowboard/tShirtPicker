37fe4fcad86c2d616f478403be72b9cf29ffb268
ddsnowboard <ddsnowboard@gmail.com>
2014-07-08 00:31:09 -0500

    Did some testing and slightly began work on GUI.
    Added 2 buttons.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sqlite3
 import datetime
 import random
+import tkinter
 db = sqlite3.connect("shirts.db")
+root = tkinter.Tk()
 c = db.cursor()
 all = '*'
 date_format = "%Y-%m-%d"
@@ -57,49 +59,63 @@ def onOpen():
 		c.execute("create table shirts (id, description, lasttime);")
 		onOpen()
 onOpen()
-while True:
-	i = input("Type PICK to pick a new shirt for today, NEW to add a shirt, UPDATE to update a shirt, LIST to list all shirts, DELETE to delete a shirt, or EXIT to exit. \n")
-	if i.lower() == "new":
-		descrip = input("Give a short description of the shirt \n")
-		last = input("When was the last time you wore the shirt, in YYYY-MM-DD format? Or leave it blank if it was never worn \n")
-		if last == '':
-			last = (datetime.datetime.today()-datetime.timedelta(days=14)).strftime(date_format)
-		shirts.append(Shirt(0, descrip, last))
-	elif i.lower() == "pick":
-		done = False
-		while not done:
-			choice = pick()
-			print("Today's shirt is " + choice.description +". Do you want to wear it? y/n")
-			n = input()
-			if n.lower() == 'y':
-				print("Ok!")
-				choice.wearToday()
-				done = True
-			elif n.lower() == 'n':
-				print("Ok then.")
-	elif i.lower() == 'update':
-		list()
-		choice = input("Type the ID of the shirt you want to change\n")
-		inp = input("Do you want to change the last worn DATE or the DESCRIPTION?\n")
-		if inp.lower()== 'description':
-			newDescrip = input("Type the new description you want\n")
-			c.execute("update shirts SET description='"+newDescrip+"' WHERE id = "+str(choice)+";")
-			db.commit()
-		elif inp.lower() == 'date':
-			newDate = input("Type the new date you want to set in YYYY-MM-DD format\n")
-			update(choice, newDate)
-	elif i.lower() == 'list':
-		list()
-	elif i.lower() == 'delete':
-		list()
-		delId = input("What is the ID of the shirt you want to delete?\n")
-		yn = input("Do you really want to delete? YES or NO\n")
-		if yn.lower() == 'yes':
-			c.execute("delete from shirts where id = "+str(delId) +";")
-			print("Successfully deleted")
-	elif i.lower() == 'exit':
-		break
-	else: 
-		print("That's not an available command. Sorry!")
+addButton = tkinter.Button(root, text="Add a shirt")
+addButton.pack(side='left', padx = 5, pady = 5)
+updateButton = tkinter.Button(root, text="Update a shirt")
+updateButton.pack(side='left', padx = 5, pady = 5)
+root.mainloop()
+
+
+
+
+
+
+
+
+# These are all for the text-based implementation, but I'm working on a GUI.
+# while True:
+	# i = input("Type PICK to pick a new shirt for today, NEW to add a shirt, UPDATE to update a shirt, LIST to list all shirts, DELETE to delete a shirt, or EXIT to exit. \n")
+	# if i.lower() == "new":
+		# descrip = input("Give a short description of the shirt \n")
+		# last = input("When was the last time you wore the shirt, in YYYY-MM-DD format? Or leave it blank if it was never worn \n")
+		# if last == '':
+			# last = (datetime.datetime.today()-datetime.timedelta(days=14)).strftime(date_format)
+		# shirts.append(Shirt(0, descrip, last))
+	# elif i.lower() == "pick":
+		# done = False
+		# while not done:
+			# choice = pick()
+			# print("Today's shirt is " + choice.description +". Do you want to wear it? y/n")
+			# n = input()
+			# if n.lower() == 'y':
+				# print("Ok!")
+				# choice.wearToday()
+				# done = True
+			# elif n.lower() == 'n':
+				# print("Ok then.")
+	# elif i.lower() == 'update':
+		# list()
+		# choice = input("Type the ID of the shirt you want to change\n")
+		# inp = input("Do you want to change the last worn DATE or the DESCRIPTION?\n")
+		# if inp.lower()== 'description':
+			# newDescrip = input("Type the new description you want\n")
+			# c.execute("update shirts SET description='"+newDescrip+"' WHERE id = "+str(choice)+";")
+			# db.commit()
+		# elif inp.lower() == 'date':
+			# newDate = input("Type the new date you want to set in YYYY-MM-DD format\n")
+			# update(choice, newDate)
+	# elif i.lower() == 'list':
+		# list()
+	# elif i.lower() == 'delete':
+		# list()
+		# delId = input("What is the ID of the shirt you want to delete?\n")
+		# yn = input("Do you really want to delete? YES or NO\n")
+		# if yn.lower() == 'yes':
+			# c.execute("delete from shirts where id = "+str(delId) +";")
+			# print("Successfully deleted")
+	# elif i.lower() == 'exit':
+		# break
+	# else: 
+		# print("That's not an available command. Sorry!")
 		
-	print("\n\n")
+	# print("\n\n")
